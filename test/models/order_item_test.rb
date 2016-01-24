eb63c622f0b9d3e8c3478fdd18846456d35a143f
Chirag Aggarwal <me@chi6rag.net>
2016-01-24 19:58:01 +0530

    Refactoring Order Item

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class OrderItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_order_item_calculates_correct_price
+    product = Product.create(name: 'example', price: 10)
+    order = Order.create
+    order_item = OrderItem.create(product_id: product.id, order_id: order.id, quantity: 3)
+
+    assert_equal(order_item.total, 30)
+  end
 end
